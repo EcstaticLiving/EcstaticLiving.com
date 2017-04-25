2284620eca85e729ca560b504c9fa1f163338203
Martin Adams <heymartinadams@users.noreply.github.com>
2017-04-25 12:42:36 -0700

    Website redesign

---
+++ index.js
@@ -0,0 +1,18 @@
+/*
+Code ©2016 Ecstatic Living Institute All rights reserved.
+Created by Conscious Apps Inc. www.consciousapps.com
+*/
+$(document).ready(function () {
+	// Load all modules
+	$.getScript('https://ecstaticliving.github.io/ecstaticliving.com/declarations.js')
+	$.getScript('https://ecstaticliving.github.io/ecstaticliving.com/init.js')
+	$.getScript('https://ecstaticliving.github.io/ecstaticliving.com/nav.js')
+	$.getScript('https://ecstaticliving.github.io/ecstaticliving.com/introModal.js')
+	$.getScript('https://ecstaticliving.github.io/ecstaticliving.com/contactModal.js')
+	$.getScript('https://ecstaticliving.github.io/ecstaticliving.com/eventinit.js')
+	$.getScript('https://ecstaticliving.github.io/ecstaticliving.com/eventnav.js')
+	$.getScript('https://ecstaticliving.github.io/ecstaticliving.com/forms.js')
+	$.getScript('https://ecstaticliving.github.io/ecstaticliving.com/regForm.js')
+	$.getScript('https://ecstaticliving.github.io/ecstaticliving.com/stripe.js')
+
+})
