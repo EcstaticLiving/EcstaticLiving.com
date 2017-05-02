2858c5555e8b353d77a1f3b078978aec1fae34da
Martin Adams <heymartinadams@users.noreply.github.com>
2017-05-02 15:34:49 -0700

    Stripe Elements works. Going LIVE.

--- index.js
+++ index.js
@@ -291,8 +291,6 @@ function eventValidation() {
 	}
 }
 $(eventFirstName + ',' + eventLastName + ',' + eventEmail + ',' + eventMobile + ',' + eventBirthdate + ',' + eventFemale + ',' + eventMale + ',' + eventOther + ',' + eventReferral + ',' + eventExperienceYes + ',' + eventExperienceNo + ',' + eventExperienceDetails + ',' + eventDietYes + ',' + eventDietNo + ',' + eventDietDetails + ',' + eventStatus + ',' + eventPartnerName + ',' + eventPartnerFemale + ',' + eventPartnerMale + ',' + eventPartnerOther + ',' + eventSelect + ',' + eventTerms + ',' + eventCard).on('change', function () {
-	console.log('Validation');
-	console.log($(eventCard).is(':checked'));
 	eventValidation()
 })
 
