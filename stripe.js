0a36c9fd45146fc2484f53ce778f1e2904062311
Martin Adams <heymartinadams@users.noreply.github.com>
2017-05-01 20:10:10 -0700

    Stripe fix.

--- stripe.js
+++ stripe.js
@@ -2,7 +2,8 @@ module.exports = function (body, callback) {
   var stripe = require('stripe')(body.secrets.elistripe)
   stripe.customers.create({
     email: body.data.stripeCustomer,
-    source: body.data.stripeToken
+    source: body.data.stripeToken,
+    description: body.data.stripeCustomer
   }).then(function(customer) {
     return stripe.charges.create({
       amount: body.data.stripeAmount,
