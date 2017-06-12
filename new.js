16b9f5a3be807ed79936681c1f429d3f66ab49be
Martin Adams <heymartinadams@users.noreply.github.com>
2017-06-12 08:53:31 -0700

    Error message on form.

--- new.js
+++ new.js
@@ -334,12 +334,12 @@ function eventValidation() {
 }
 function eventCorrection() {
 	if (!$(eventFemale).is(':checked') && !$(eventMale).is(':checked') && !$(eventOther).is(':checked')) { $(eventFemale).focus() }
-	if ($(eventBirthdate).val() === '') { $(eventBirthdate).css('border', '1px solid #800000'); $(eventBirthdate).focus() }
-	if ($(eventMobile).val() === '') { $(eventMobile).css('border', '1px solid #800000'); $(eventMobile).focus() }
-	if ($(eventEmail).val() === '') { $(eventEmail).css('border', '1px solid #800000'); $(eventEmail).focus() }
-	if ($(eventLastName).val() === '') { $(eventLastName).css('border', '1px solid #800000'); $(eventLastName).focus() }
-	if ($(eventFirstName).val() === '') { $(eventFirstName).css('border', '1px solid #800000'); $(eventFirstName).focus() }
-	// if ($(xxx).val() === '') { $(xxx).css('border', '1px solid #800000'); $(xxx).focus() }
+	if ($(eventBirthdate).val() === '') { $(eventBirthdate).css({ 'border-color': '#800000', 'background-color': '#fdd' }); $(eventBirthdate).focus() }
+	if ($(eventMobile).val() === '') { $(eventMobile).css({ 'border-color': '#800000', 'background-color': '#fdd' }); $(eventMobile).focus() }
+	if ($(eventEmail).val() === '') { $(eventEmail).css({ 'border-color': '#800000', 'background-color': '#fdd' }); $(eventEmail).focus() }
+	if ($(eventLastName).val() === '') { $(eventLastName).css({ 'border-color': '#800000', 'background-color': '#fdd' }); $(eventLastName).focus() }
+	if ($(eventFirstName).val() === '') { $(eventFirstName).css({ 'border-color': '#800000', 'background-color': '#fdd' }); $(eventFirstName).focus() }
+	// if ($(xxx).val() === '') { $(xxx).css({ 'border-color': '#800000', 'background-color': '#fdd' }); $(xxx).focus() }
 }
 
 // PARTNERSHIP
@@ -689,6 +689,7 @@ if (page === 'Event' || page === 'Custom') {
 $(`${payButton}`).on('click', function(e) {
 	e.preventDefault()
 	if (!eventValidation()) {
+		eventCorrection()
 		$('#card-errors').text('Oops! There’s some missing information.')
 		return false
 	}
