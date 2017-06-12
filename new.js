c241cb1c031b26643398dcba14fe300d265e663b
Martin Adams <heymartinadams@users.noreply.github.com>
2017-06-12 14:15:12 -0700

    Error message on form.

--- new.js
+++ new.js
@@ -304,7 +304,7 @@ function eventCorrection() {
 		if ($(eventExperienceYes).is(':checked') && $(eventExperienceDetails).val() === '') { $(eventExperienceDetails).css(errorInput); } else { $(eventExperienceDetails).css(clearInput); }
 		if (!$(eventFemale).is(':checked') && !$(eventMale).is(':checked') && !$(eventOther).is(':checked')) { $(eventGenderValidation).css(errorRadio); } else { $(eventGenderValidation).css(clearRadio); }
 		$eventForm.parsley().validate()
-		console.log(document.getElementsByClassName('__PrivateStripeElement-input')[0])
+		console.log(document.getElementsByClassName('__PrivateStripeElement-input')[0].value)
 		if ($('#card-element').text() === '') { $('#card-element').css(errorInput) } else { $('#card-element').css(clearInput) }
 		return false
 	}
