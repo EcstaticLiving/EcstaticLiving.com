6904cc05740620e41d486caf22dada17a3311ffb
Martin Adams <heymartinadams@users.noreply.github.com>
2017-06-12 09:24:12 -0700

    Error message on form.

--- new.js
+++ new.js
@@ -282,7 +282,7 @@ function participants() {
 // FORM VALIDATION
 function eventCorrection() {
 	if (payButtonClicked) {
-		if (!$(eventFemale).is(':checked') && !$(eventMale).is(':checked') && !$(eventOther).is(':checked')) { $('#event-gender').css({ 'border': '1px solid #800000', 'background-color': '#fdd' }); }
+		if (!$(eventFemale).is(':checked') && !$(eventMale).is(':checked') && !$(eventOther).is(':checked')) { $('#event-gender').css({ 'border-bottom': '1px solid #800000', 'background-color': '#fdd' }); }
 			else { $('#event-gender').css({ 'border': 'none', 'background-color': 'transparent' }); }
 		if ($(eventBirthdate).val() === '') { $(eventBirthdate).css({ 'border-color': '#800000', 'background-color': '#fdd' }); $(eventBirthdate).focus() }
 			else { $(eventBirthdate).css({ 'border-color': '#ccc', 'background-color': '#fff' }) }
