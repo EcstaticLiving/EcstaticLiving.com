941e50c1d00f260f179d18ae63bf50b4afe845e5
Martin Adams <heymartinadams@users.noreply.github.com>
2017-07-07 08:28:32 -0700

    Testing form submission.

--- test.js
+++ test.js
@@ -739,7 +739,7 @@ $('#button-stripe-error').on('click', function() {
 })
 
 $(payButton).on('click', function(e) {
-	e.preventDefault()
+	// e.preventDefault()
 	payButtonClicked = true
 	if (!eventValidation()) { return false }
 	saveForm(page)
