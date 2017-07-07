8027d338e7791689a19aa1aaf3bc2859877e5f69
Martin Adams <heymartinadams@users.noreply.github.com>
2017-07-06 17:04:34 -0700

    Testing form submission.

--- test.js
+++ test.js
@@ -680,7 +680,7 @@ function stripeTokenHandler(token, data) {
 	.then(function (res) {
 		$('.notification-modal.processing').hide()
 		if (page === 'Event') {
-			document.getElementById("wf-form-Event-Registration").submit()
+			$eventForm.submit()
 			// window.location.href = `${siteUrl}registered`
 		} else if (page === 'Custom') {
 			$customForm.submit()
