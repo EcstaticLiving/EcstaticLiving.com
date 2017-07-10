853341f9dc08720b177a82d07cd83530739d7860
Martin Adams <heymartinadams@users.noreply.github.com>
2017-07-09 22:05:21 -0700

    Abandoned reg feature.

--- index.js
+++ index.js
@@ -581,8 +581,10 @@ if (page === 'Event') {
 		eventValidation()
 	})
 	// Abandoned reg
-	window.onbeforeunload = function(e) {
+	window.unload = function(e) {
+		console.log('unload');
 		if (!eventValidation() && $(eventFirstName).val() !== 0 && $(eventLastName).val() !== 0 && ($(eventEmail).val() !== 0 || $(eventMobile).val() !== 0)) {
+			console.log('form submit');
 			$eventForm.submit()
 		}
 		return null
