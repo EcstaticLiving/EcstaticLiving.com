7d466ab42002b76885b29f7b79d77e14624fe57a
Martin Adams <heymartinadams@users.noreply.github.com>
2017-10-05 10:54:11 -0700

    LIVE and TEST js file.

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ Webflow.push(function () {
 	script.type = 'text/javascript'
 	// Load test code
 	if (window.location.href.indexOf('ecstaticliving.webflow.io') > -1) {
-		script.src = 'test.js'
+		script.src = 'https://ecstaticliving.github.io/ecstaticliving.com/test.js'
 	} else
 	// Load public code
 	if (window.location.href.indexOf('ecstaticliving.com') > -1) {
-		script.src = 'live.js'
+		script.src = 'https://ecstaticliving.github.io/ecstaticliving.com/live.js'
 	}
 	document.getElementsByTagName('head')[0].appendChild(script)
 })
