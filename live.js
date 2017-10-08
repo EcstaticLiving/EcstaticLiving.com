4d2f4847d376b241a5b30ad283d6177295ec0324
Martin Adams <heymartinadams@users.noreply.github.com>
2017-10-08 09:06:21 -0700

    Going LIVE.

--- live.js
+++ live.js
@@ -143,7 +143,7 @@ $('.button.contact').on('click', function() {
 
 // Countries
 var countriesfile = "United States, Canada, Afghanistan, Albania, Algeria, Andorra, Angola, Antigua & Deps, Argentina, Armenia, Australia, Austria, Azerbaijan, Bahamas, Bahrain, Bangladesh, Barbados, Belarus, Belgium, Belize, Benin, Bhutan, Bolivia, Bosnia Herzegovina, Botswana, Brazil, Brunei, Bulgaria, Burkina, Burma, Burundi, Cambodia, Cameroon, Cape Verde, Central African Rep, Chad, Chile, People's Republic of China, Republic of China, Colombia, Comoros, Democratic Republic of the Congo, Republic of the Congo, Costa Rica, Croatia, Cuba, Cyprus, Czech Republic, Danzig, Denmark, Djibouti, Dominica, Dominican Republic, East Timor, Ecuador, Egypt, El Salvador, Equatorial Guinea, Eritrea, Estonia, Ethiopia, Fiji, Finland, France, Gabon, Gaza Strip, The Gambia, Georgia, Germany, Ghana, Greece, Grenada, Guatemala, Guinea, Guinea-Bissau, Guyana, Haiti, Holy Roman Empire, Honduras, Hungary, Iceland, India, Indonesia, Iran, Iraq, Republic of Ireland, Israel, Italy, Ivory Coast, Jamaica, Japan, Jonathanland, Jordan, Kazakhstan, Kenya, Kiribati, North Korea, South Korea, Kosovo, Kuwait, Kyrgyzstan, Laos, Latvia, Lebanon, Lesotho, Liberia, Libya, Liechtenstein, Lithuania, Luxembourg, Macedonia, Madagascar, Malawi, Malaysia, Maldives, Mali, Malta, Marshall Islands, Mauritania, Mauritius, Mexico, Micronesia, Moldova, Monaco, Mongolia, Montenegro, Morocco, Mount Athos, Mozambique, Namibia, Nauru, Nepal, Newfoundland, Netherlands, New Zealand, Nicaragua, Niger, Nigeria, Norway, Oman, Ottoman Empire, Pakistan, Palau, Panama, Papua New Guinea, Paraguay, Peru, Philippines, Poland, Portugal, Prussia, Qatar, Romania, Rome, Russian Federation, Rwanda, St Kitts & Nevis, St Lucia, Saint Vincent & the, Grenadines, Samoa, San Marino, Sao Tome & Principe, Saudi Arabia, Senegal, Serbia, Seychelles, Sierra Leone, Singapore, Slovakia, Slovenia, Solomon Islands, Somalia, South Africa, Spain, Sri Lanka, Sudan, Suriname, Swaziland, Sweden, Switzerland, Syria, Tajikistan, Tanzania, Thailand, Togo, Tonga, Trinidad & Tobago, Tunisia, Turkey, Turkmenistan, Tuvalu, Uganda, Ukraine, United Arab Emirates, United Kingdom, Uruguay, Uzbekistan, Vanuatu, Vatican City, Venezuela, Vietnam, Yemen, Zambia, Zimbabwe";
-var countries = countriesfile.split(', ');
+var countries = countriesfile.split(', ')
 for(var i = 0; i < countries.length; i++) {
 	if (page === 'Event' || page === 'Custom') {
 		if (countries[i] === 'United States') {
@@ -239,6 +239,8 @@ eventDepositDate = $('#event-deposit-date').text()
 // Event variables
 var payButtonClicked = false;
 const payButton = '#payment-button',
+eventRegForm = '.event-container.reg-form',
+eventInviteButton = '#event-invitecode-button',
 eventInviteBox = '#event-invitecode-box',
 eventInviteCode = '#event-invitecode-code',
 eventInvitePass = '#event-invitecode-pass',
@@ -411,13 +413,16 @@ function eventAffiliateShowErrors() {
 	if ($(eventInviteBox).is(':visible')) {
 		if ($(eventInviteCode).val().length > 0) {
 			if (!eventAffiliateValidation()) {
+				$(eventRegForm).hide()
 				eventInvitePassHide()
 				eventInviteFailShow()
 			} else {
+				$(eventRegForm).show()
 				eventInviteFailHide()
 				eventInvitePassShow()
 			}
 		} else {
+			$(eventRegForm).hide()
 			eventInvitePassHide()
 			eventInviteFailHide()
 		}
@@ -744,10 +749,32 @@ function resetEventForm() {
 
 
 // EVENT FORM: BEGIN SEQUENCE
+if (page === 'Event' || page === 'Custom') {
+
+	// Prevent accidental submission of form through 'enter' key
+	$('.form-input').keypress(function (e) {
+		if (e.which === 13) {
+			e.preventDefault()
+			return false
+		}
+	})
+
+}
+
 if (page === 'Event') {
 
 	// EVENT FORM ONCHANGE EVENTS
 	if ($(eventInviteBox).is(':visible')) {
+		// If private event, hide registration form until successful invite code has been entered
+		$(eventRegForm).hide()
+		$(eventInviteButton).on('click', function () {
+			// Show errors, if any
+			eventAffiliateShowErrors()
+			// Adjust prices
+			setEventPrices()
+			// Validate form
+			eventFormValidation()
+		})
 		$(eventInviteCode).on('change', function () {
 			// Show errors, if any
 			eventAffiliateShowErrors()
@@ -756,6 +783,9 @@ if (page === 'Event') {
 			// Validate form
 			eventFormValidation()
 		})
+	} else {
+		// Make sure event reg form is shown if not private event
+		$(eventRegForm).show()
 	}
 	$(eventFirstName).on('change', function () {
 		$(billingFirstName).val($(eventFirstName).val())
@@ -941,7 +971,7 @@ function conversion(e, n) {
 		var a = $(o),
 			s = a.attr("type"),
 			u = a.attr("data-name") || a.attr("name") || "Field " + (r + 1),
-			l = a.val();
+			l = a.val()
 		if ("checkbox" === s && (l = a.is(":checked")), "radio" === s) {
 			if (null === n[u] || "string" == typeof n[u]) return;
 			l = e.find('input[name="' + a.attr("name") + '"]:checked').val() || null
@@ -998,10 +1028,9 @@ function stripeTokenHandler(token, data) {
 				dataType: 'json'
 			})
 			.then(function(response) {
-				console.log(response);
+				console.log(response)
 				window.location.href = `${siteUrl}registered`
 			})
-			// $eventForm.submit()
 		} else if (page === 'Custom') {
 			$customForm.submit()
 			window.location.href = `${siteUrl}success`
@@ -1056,6 +1085,10 @@ $('#button-stripe-error').on('click', function() {
 })
 
 $(payButton).on('click', function(e) {
+	// Prevent accidental submission of form through 'enter' key
+	if (e.which === 13) {
+		return false
+	}
 	e.preventDefault()
 	if (page === 'Event') {
 		if (!eventFormValidation()) {
@@ -1087,7 +1120,7 @@ $(payButton).on('click', function(e) {
 		} else {
 			$('#trafficsource').val('ELI')
 		}
-		console.log($('#trafficsource').val());
+		console.log($('#trafficsource').val())
 		count = $(eventSelect).prop('selectedIndex') - 1
 		chargeAmount = $(eventDepositDeposit).is(':checked') ? eventDepositPrice * 100 : $(eventSelect).val() * 100
 		const eventDeposit = $(eventDepositDeposit).is(':checked') ? 'DEPOSIT' : 'FULL'
