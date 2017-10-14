e9fe6f32bba6d851d07edcdea9dfbb6c4e4597ef
Martin Adams <heymartinadams@users.noreply.github.com>
2017-10-14 14:50:53 -0700

    Browser validation.

--- test.js
+++ test.js
@@ -98,9 +98,9 @@ if (window.location.href.indexOf('/contact') > -1) {
 // Browser detection courtesy of: https://stackoverflow.com/questions/9847580/how-to-detect-safari-chrome-ie-firefox-and-opera-browser
 const ua = window.navigator.userAgent
 
-console.log(ua.split('Firefox/')[1]);
+console.log(Number.parseInt(ua.split('Firefox/')[1]));
 const isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification))
-if ((page === 'Event' || page === 'Custom') && !is.edge() && !is.chrome() && (!is.firefox() || ua.split('Firefox/')[1] >= 34) && !is.opera() && !isSafari) {
+if ((page === 'Event' || page === 'Custom') && !is.edge() && !is.chrome() && (!is.firefox() || Number.parseInt(ua.split('Firefox/')[1]) >= 34) && !is.opera() && !isSafari) {
 	window.alert('This page does not work on this browser. Please use a different browser.')
 }
 
