54e56d651aba1d43d28734ca7c49c348f76a9104
Martin Adams <heymartinadams@users.noreply.github.com>
2017-10-14 11:29:28 -0700

    Disabled Internet Explorer and Kindle.

--- live.js
+++ live.js
@@ -76,9 +76,9 @@ if (window.location.href.indexOf('/contact') > -1) {
 	page = 'Contact'
 }
 
-const userAgent = navigator.userAgent
-const isKindle = /Kindle/i.test(userAgent) || /Silk/i.test(userAgent) || /KFTT/i.test(userAgent) || /KFOT/i.test(userAgent) || /KFJWA/i.test(userAgent) || /KFJWI/i.test(userAgent) || /KFSOWI/i.test(userAgent) || /KFTHWA/i.test(userAgent) || /KFTHWI/i.test(userAgent) || /KFAPWA/i.test(userAgent) || /KFAPWI/i.test(userAgent)
-const isIE = /MSIE 10/i.test(userAgent) || /MSIE 9/i.test(userAgent) || /rv:11.0/i.test(userAgent)
+var userAgent = navigator.userAgent
+var isKindle = /Kindle/i.test(userAgent) || /Silk/i.test(userAgent) || /KFTT/i.test(userAgent) || /KFOT/i.test(userAgent) || /KFJWA/i.test(userAgent) || /KFJWI/i.test(userAgent) || /KFSOWI/i.test(userAgent) || /KFTHWA/i.test(userAgent) || /KFTHWI/i.test(userAgent) || /KFAPWA/i.test(userAgent) || /KFAPWI/i.test(userAgent)
+var isIE = /MSIE 10/i.test(userAgent) || /MSIE 9/i.test(userAgent) || /rv:11.0/i.test(userAgent)
 if ((page === 'Event' || page === 'Custom') && (isKindle || isIE)) {
 	window.alert('This page does not work on this browser. Please use a different browser.')
 }
