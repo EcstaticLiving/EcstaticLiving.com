af8eff3ed109c00fe4b3ed8f823e34b619d8af39
Martin Adams <heymartinadams@users.noreply.github.com>
2017-10-14 11:42:37 -0700

    Disabled Internet Explorer and Kindle.

--- live.js
+++ live.js
@@ -77,7 +77,7 @@ if (window.location.href.indexOf('/contact') > -1) {
 }
 
 const userAgent = navigator.userAgent
-const browserKindle = /Kindle|Silk|KFAPW|KFARWI|KFASWI|KFFOWI|KFJW|KFMEWI|KFOT|KFS‌​AW|KFSOWI|KFTBW|KFTH‌​W|KFTT|WFFOWI/i.test‌​(userAgent)
+const browserKindle = /Kindle/i.test(userAgent) || /Silk/i.test(userAgent) || /KFTT/i.test(userAgent) || /KFOT/i.test(userAgent) || /KFJWA/i.test(userAgent) || /KFJWI/i.test(userAgent) || /KFSOWI/i.test(userAgent) || /KFTHWA/i.test(userAgent) || /KFTHWI/i.test(userAgent) || /KFAPWA/i.test(userAgent) || /KFAPWI/i.test(userAgent)
 const browserIE = /MSIE 10/i.test(userAgent) || /MSIE 9/i.test(userAgent) || /rv:11.0/i.test(userAgent)
 if ((page === 'Event' || page === 'Custom') && (browserKindle || browserIE)) {
 	window.alert('This page does not work on this browser. Please use a different browser.')
