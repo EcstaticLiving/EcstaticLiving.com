b10667668a91544dbb9f7d6e15792fd6ac40fba5
heymartinadams <heymartinadams@users.noreply.github.com>
2018-01-12 12:18:57 -0800

    Error on enter key.

--- test.js
+++ test.js
@@ -498,7 +498,6 @@ function showErrorsInEventForm() {
 	if (!$(eventExperienceYes).is(':checked') && !$(eventExperienceNo).is(':checked')) { $(eventExperienceParsleyError).css(errorRadio); } else { $(eventExperienceParsleyError).css(clearRadio); }
 	if ($(eventExperienceYes).is(':checked') && $(eventExperienceDetails).val() === '') { $(eventExperienceDetails).css(errorInput); } else { $(eventExperienceDetails).css(clearInput); }
 	if (!$(eventFemale).is(':checked') && !$(eventMale).is(':checked') && !$(eventOther).is(':checked')) { $(eventGenderValidation).css(errorRadio); } else { $(eventGenderValidation).css(clearRadio); }
-	console.log('parsley validation');
 	$eventForm.parsley().validate()
 }
 
@@ -1126,6 +1125,11 @@ if (page === 'Event' || page === 'Custom') {
 
 $('#button-stripe-error').on('click', function() {
 	$('.notification-modal.error').hide()
+})
+
+$($eventForm).on('submit', function(e) {
+	console.log(e);
+	console.log('form submitted');
 })
 
 $(payButton).on('click', function(e) {
