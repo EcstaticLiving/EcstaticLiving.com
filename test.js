e5cf9505b25e24008e32ce0da5b137eb39d8fbd4
heymartinadams <heymartinadams@users.noreply.github.com>
2018-01-12 12:24:33 -0800

    Error on enter key.

--- test.js
+++ test.js
@@ -1130,6 +1130,8 @@ $('#button-stripe-error').on('click', function() {
 $($eventForm).on('submit', function(e) {
 	console.log(e);
 	console.log('form submitted');
+	debugger
+	return false
 })
 
 $(payButton).on('click', function(e) {
