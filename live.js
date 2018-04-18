7e55afa723400da476eec99a487227c54ae83ad9
Martin Adams <heymartinadams@users.noreply.github.com>
2018-04-18 17:35:05 +0200

    Fixing fade/scroll glitch.

--- live.js
+++ live.js
@@ -561,11 +561,11 @@ function hidePartner() {
 }
 // Previous Experience
 function showExperience() {
-	$(eventExperienceContainer).animate({ width: '100%', height: '100%' }, 100)
+	$(eventExperienceContainer).animate({ opacity: 1, width: '100%', height: '100%' }, 100)
 }
 function hideExperience() {
 	$(eventExperienceDetails).val('')
-	$(eventExperienceContainer).animate({ width: '0%', height: '0%' }, 100)
+	$(eventExperienceContainer).animate({ opacity: 0, width: '0%', height: '0%' }, 100)
 }
 // Dietary Needs
 function showDiet() {
