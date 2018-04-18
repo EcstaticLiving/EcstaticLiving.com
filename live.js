e125f55998ffefd33f3c5cee36e8e6a558d2e690
Martin Adams <heymartinadams@users.noreply.github.com>
2018-04-18 16:33:38 +0200

    Fixing fade/scroll glitch.

--- live.js
+++ live.js
@@ -562,7 +562,7 @@ function hidePartner() {
 // Previous Experience
 function showExperience() {
 	$(eventExperienceContainer).show()
-	console.log(document.getElementsByClassName('event-container.experience')[0])
+	console.log(document.getElementsByClassName('event-container experience')[0])
 }
 function hideExperience() {
 	$(eventExperienceDetails).val('')
