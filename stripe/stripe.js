41e136efcf87a35fa5bf1df0f7ec2bbace82ae4b
heymartinadams <heymartinadams@users.noreply.github.com>
2019-01-07 15:31:17 -0800

    Updating Stripe to search for existing customer instead of creating new customer each time.

--- stripe/stripe.js
+++ stripe/stripe.js
@@ -1,21 +1,33 @@
 // Uses webtask.io
-// To create a server, enter the following code on the CLI:
+// To create a server, navigate to the /stripe folder and enter the following code on the CLI:
 // wt create stripe.js --secret elistripelive=STRIPESECRET --parse-body --meta 'wt-node-dependencies'='{"stripe":"6.1.0"}'
 // Then add the resulting URL to the Stripe url in the index.js file.
-module.exports = function (body, callback) {
+module.exports = (body, callback) => {
+
   var stripe = require('stripe')(body.secrets.elistripelive)
+
   console.log(body.data)
-  stripe.customers.create({
-    email: body.data.stripeCustomer,
-    source: body.data.stripeToken,
-    description: body.data.stripeCustomer
-  })
-  .then(function(customer) {
-    return stripe.charges.create({
+
+  let existingCustomers = await stripe.customers.list({ email : body.data.stripeCustomer })
+
+  console.log(existingCustomers)
+
+  // Customer already exists
+  if (existingCustomers.data.length) {
+    console.log(existingCustomers.data[0])
+  }
+  // Create new customer
+  else {
+    stripe.customers.create({
+      email: body.data.stripeCustomer,
+      source: body.data.stripeToken,
+      description: body.data.stripeCustomer
+    })
+    .then(customer => stripe.charges.create({
       amount: body.data.stripeAmount,
       currency: 'usd',
       customer: customer.id,
       description: body.data.stripeCharge
-    }, callback);
-  })
-};
+    }, callback))
+  }
+}
