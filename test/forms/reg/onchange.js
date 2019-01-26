55cc72dcc106efdaf3d060589a0568a7599bd0a5
Martin Adams <heymartinadams@users.noreply.github.com>
2019-01-26 06:52:46 -0800

    Simplifying code in progress.

--- test/forms/reg/onchange.js
+++ test/forms/reg/onchange.js
@@ -42,27 +42,33 @@ onChange(eventFirstName, () => setValue(billingFirstName, getValue(eventFirstNam
 onChange(eventLastName, () => setValue(billingLastName, getValue(eventLastName)))
 
 // Details
-[eventExperienceYes, eventExperienceNo].forEach(elem => onChange(elem, () => {
-	if (isChecked(eventExperienceYes)) showAndScrollTo(eventExperienceContainer)
-	if (isChecked(eventExperienceNo)) {
-		emptyValue(eventExperienceDetails)
-		hideElement(eventExperienceContainer)
-	}
-}))
-[eventDietYes, eventDietNo].forEach(elem => onChange(elem, () => {
-	if (isChecked(eventDietYes)) showAndScrollTo(eventDietContainer)
-	if (isChecked(eventDietNo)) {
-		emptyValue(eventDietDetails)
-		hideElement(eventDietContainer)
-	}
-}))
-[eventSpecialYes, eventSpecialNo].forEach(elem => onChange(elem, () => {
-	if (isChecked(eventSpecialYes)) showAndScrollTo(eventSpecialContainer)
-	if (isChecked(eventSpecialNo)) {
-		emptyValue(eventSpecialDetails)
-		hideElement(eventSpecialContainer)
-	}
-}))
+for (elem of [eventExperienceYes, eventExperienceNo]) {
+	onChange(elem, () => {
+		if (isChecked(eventExperienceYes)) showAndScrollTo(eventExperienceContainer)
+		if (isChecked(eventExperienceNo)) {
+			emptyValue(eventExperienceDetails)
+			hideElement(eventExperienceContainer)
+		}
+	})
+}
+for (elem of [eventDietYes, eventDietNo]) {
+	onChange(elem, () => {
+		if (isChecked(eventDietYes)) showAndScrollTo(eventDietContainer)
+		if (isChecked(eventDietNo)) {
+			emptyValue(eventDietDetails)
+			hideElement(eventDietContainer)
+		}
+	})
+}
+for (elem of [eventSpecialYes, eventSpecialNo]) {
+	onChange(elem, () => {
+		if (isChecked(eventSpecialYes)) showAndScrollTo(eventSpecialContainer)
+		if (isChecked(eventSpecialNo)) {
+			emptyValue(eventSpecialDetails)
+			hideElement(eventSpecialContainer)
+		}
+	})
+}
 
 // Partner
 onChange(eventStatus, () => participants() === 2 ? showPartner() : hidePartner())
@@ -70,14 +76,17 @@ onChange(eventPayBoth, () => setEventPrices())
 onChange(eventPayMe, () => setEventPrices())
 
 // All reg fields
+for (elem of
 [
 	eventFirstName, eventLastName, eventEmail, eventMobile, eventBirthdate, eventFemale, eventMale, eventOther, eventReferral, eventExperienceYes, eventExperienceNo, eventExperienceDetails, eventDietYes, eventDietNo, eventDietDetails, eventSpecialYes, eventSpecialNo, eventSpecialDetails, eventStatus, eventPartnerFirstName, eventPartnerLastName, eventPartnerFemale, eventPartnerMale, eventPartnerOther, eventPayBoth, eventPayMe, eventSelect, eventTerms, billingFirstName, billingLastName, billingStreet, billingCity, billingState, billingPostal, billingCountry
-].forEach(elem => onChange(elem, () => {
-	// Save form whenever a single field has changed...
-	saveForm(page())
-	// ...and keep validating form to either activate or deactivate Pay Now button.
-	formValidation()
-}))
+]) {
+	onChange(elem, () => {
+		// Save form whenever a single field has changed...
+		saveForm(page())
+		// ...and keep validating form to either activate or deactivate Pay Now button.
+		formValidation()
+	})
+}
 
 // To display grand total (optional feature)
 [eventSelect, eventDepositFull, eventDepositDeposit].forEach(elem => onChange(elem, () => {
