40cdeaae10f58e58b400650e7386546b429d6010
Martin Adams <heymartinadams@users.noreply.github.com>
2019-01-26 19:35:13 -0800

    Update.

--- test/forms/webflow.js
+++ test/forms/webflow.js
@@ -7,8 +7,10 @@ const verification = (t, e, n, i) => {
 
 const conversion = (e, n) => {
 	let i = null
-	console.log(e.querySelectorAll('input:not([type=\'submit\'])'))
-	return n = n || {}, e.find('input:not([type=\'submit\'])').each((r, o) => {
+	console.log(e[0].querySelectorAll('input:not([type=\'submit\'])'))
+	console.log(e.find(':input:not([type=\'submit\'])'))
+	console.log(e.find('input:not([type=\'submit\'])'))
+	return n = n || {}, e.find(':input:not([type=\'submit\'])').each((r, o) => {
 		let a = getElementById(o),
 			s = getAttribute(a,'type'),
 			u = getAttribute(a,'data-name') || getAttribute(a,'name') || 'Field ' + (r + 1),
