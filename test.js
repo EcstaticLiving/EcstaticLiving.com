86321abfb014b3d23326b1c94ef1bd5768be5af9
Martin Adams <heymartinadams@users.noreply.github.com>
2019-01-29 10:49:57 -0600

    Err status text updated.

--- test.js
+++ test.js
@@ -1401,7 +1401,7 @@ function stripeSourceHandler(data) {
 								ERROR: 'Did not receive successful payment confirmation from Stripe on previous registration made by ' + formData.fields.Party + '. Staff, please verify that payment went through. Customer was informed that registration completed successfully. If Stripe payment exists, no further action has to be taken; if Stripe payment is missing, please reach out to customer for payment.'
 							}
 							: {
-								ERROR: 'The following error occurred on the previous registration made by ' + formData.fields.Party + '. Customer was notified of error, and payment likely did not go through. Error: ' + err
+								ERROR: 'The following error occurred on the previous registration made by ' + formData.fields.Party + '. Customer was notified of error, and payment likely did not go through. Error: ' + err.statusText
 							}
 						$.ajax({
 							type: 'POST',
