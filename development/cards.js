13ccdd25fe87ed1766d2245e505603e18ad2aaa0
Martin Adams <heymartinadams@users.noreply.github.com>
2019-02-13 21:07:43 -0800

    Minor change.

--- development/cards.js
+++ development/cards.js
@@ -5,7 +5,10 @@ const calendarCardElements = [
   document.getElementsByClassName('calendar-detail-venue'),
   document.getElementsByClassName('calendar-detail-location'),
   document.getElementsByClassName('calendar-detail-dates'),
-  document.getElementsByClassName('card-button')
+  document.getElementsByClassName('card-button'),
+  document.getElementsByClassName('ribbon blue'),
+  document.getElementsByClassName('ribbon green'),
+  document.getElementsByClassName('ribbon red')
 ]
 const calendarCards = document.getElementsByClassName('card')
 const device = getDevice()
