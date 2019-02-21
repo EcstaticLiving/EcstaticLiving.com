48b777398fd1a7dbd9330a2b8c0430001eae6173
Martin Adams <heymartinadams@users.noreply.github.com>
2019-02-20 20:42:22 -0800

    Added hero image onload.

--- development/onload.js
+++ development/onload.js
@@ -1,5 +1,5 @@
-const MAX_SLIDES = 2
-const slideClasses = ['one', 'two', 'three', 'four', 'five', 'six']
+const MAX_SLIDES = 1
+const literalNumbers = ['one', 'two', 'three', 'four', 'five', 'six']
 
 // If window orientation changes
 const getDevice = () => {
@@ -26,17 +26,17 @@ if (window.location.pathname === '/') {
 		slideNumber = Math.floor(Math.random() * MAX_SLIDES) + 1
 	}
 	// Don’t use same slide
-	while (slideNumber === 1 && hero.classList.contains(slideClasses[slideNumber - 1]))
-	for (let slideClass in slideClasses) {
+	while (slideNumber === 1 && hero.classList.contains(literalNumbers[slideNumber - 1]))
+	for (let slideClass in literalNumbers) {
 		hero.classList.remove(slideClass)
 	}
-	hero.classList.add(slideClasses[slideNumber - 1])
+	hero.classList.add(literalNumbers[slideNumber - 1])
 	// Slide in Hero Text
 	const heroTitle = [
 		document.getElementsByClassName('hero-slide-title one')[slideNumber],
 		document.getElementsByClassName('hero-slide-title two')[slideNumber],
 		document.getElementsByClassName('hero-slide-title three')[slideNumber]
 	]
-	console.log(heroTitle)
+	console.log(slideNumber, heroTitle)
 	heroTitle.forEach(title => title.classList.add('fade-slide'))	
 }
