7ae15986e3bc4c9ebb1ac70c948af818cb38e44d
Martin Adams <heymartinadams@users.noreply.github.com>
2019-02-20 21:58:48 -0800

    Added hero image onload.

--- development/onload.js
+++ development/onload.js
@@ -1,14 +1,15 @@
 const MAX_SLIDES = 2
-const literalNumbers = ['one', 'two', 'three', 'four', 'five', 'six']
 
 // Preload images and randomly rotate
 if (window.location.pathname === '/') {
 	const hero = document.getElementsByClassName('hero-slide')[0]
 	hero.classList.add('fade')
 	// Slide in Hero Text
-	for (let i = 0; i < 3; i++) {
-		const title = document.getElementsByClassName('hero-slide-title ' + literalNumbers[i])[0]
-		console.log(title)
-		title.classList.add('fade-slide')
-	}
+	setTimeout(() => {
+		const literalNumbers = ['one', 'two', 'three', 'four', 'five', 'six']
+		for (let i = 0; i < 3; i++) {
+			const title = document.getElementsByClassName('hero-slide-title ' + literalNumbers[i])[0]
+			title.classList.add('fade-slide')
+		}
+	}, 500)
 }
