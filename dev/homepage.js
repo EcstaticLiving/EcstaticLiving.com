709ad106d5d97a0136a7c6ff91d0eee21a2a9cda
Martin Adams <heymartinadams@users.noreply.github.com>
2019-02-22 09:38:01 -0800

    Hero.

--- dev/homepage.js
+++ dev/homepage.js
@@ -1,48 +1,41 @@
 // Load all tabs
-const tabs = getElementsByClassName('hero-slide-container')
+const tabs = getElementsByClassName('hero-tab')
 const literalNumbers = ['one', 'two', 'three', 'four', 'five', 'six']
 
 // Preload images and randomly rotate
-const fadeInSlide = (tabNumber, initiate) => {
+const fadeInTab = tabIndex => {
 	// Fade in Hero Image
-	if (initiate) {
-		const hero = getElementByClassName('hero-slide', tabNumber)
-		hero.classList.add('fade')
-	}
+	tabs[tabIndex].classList.add('fade-in')
 	// Slide in Hero Text
 	setTimeout(() => {
 		for (let i = 0; i < 3; i++) {
-			const title = getElementByClassName('hero-title ' + literalNumbers[i], tabNumber)
+			const title = getElementByClassName('hero-title ' + literalNumbers[i], tabIndex)
 			title.classList.add('fade-slide')
 		}
 	}, 500)
 	// Slide in Hero Button
 	setTimeout(() => {
-		const heroButton = getElementByClassName('hero-button', tabNumber)
+		const heroButton = getElementByClassName('hero-button', tabIndex)
 		heroButton.classList.add('fade-slide')
 	}, 800)
 	// Slide in Hero Arrows
 	setTimeout(() => {
-		const heroArrows = getElementByClassName('hero-arrows', tabNumber)
+		const heroArrows = getElementByClassName('hero-arrows', tabIndex)
 		heroArrows.classList.add('fade-slide')
 	}, 1100)
 }
 
-const resetSlide = tabNumber => {
-	// Hide Hero Image
-	// const hero = getElementByClassName('hero-slide', tabNumber)
-	// hero.classList.remove('fade')
-	// hero.classList.remove('w-dyn-item')
+const resetSlide = tabIndex => {
 	// Hide Hero Text
 	for (let i = 0; i < 3; i++) {
-		const title = getElementByClassName('hero-title ' + literalNumbers[i], tabNumber)
+		const title = getElementByClassName('hero-title ' + literalNumbers[i], tabIndex)
 		title.classList.remove('fade-slide')
 	}
 	// Hide Hero Button
-	const heroButton = getElementByClassName('hero-button', tabNumber)
+	const heroButton = getElementByClassName('hero-button', tabIndex)
 	heroButton.classList.remove('fade-slide')
 	// Hide Hero Arrows
-	const heroArrows = getElementByClassName('hero-arrows', tabNumber)
+	const heroArrows = getElementByClassName('hero-arrows', tabIndex)
 	heroArrows.classList.remove('fade-slide')
 }
 
@@ -55,29 +48,25 @@ if (window.location.pathname === '/') {
 	const rightArrows = getElementsByClassName('hero-arrow right')
 	for (let i = 0; i < rightArrows.length; i++) {
 		rightArrows[i].addEventListener('click', () => {
-			// Get current and next tab
-			let tabNumber
+			// Get current and next image
+			let tabIndex
 			for (let i = 0; i < tabs.length; i++) {
 				if (tabs[i].classList.contains('w--tab-active')) {
-					tabNumber = i === tabs.length - 1
+					tabIndex = i === tabs.length - 1
 						? { current: i, next: 0 }
 						: { current: i, next: i + 1 }
 				}
 			}
-			const heroTabs = getElementsByClassName('hero-tab')
-			heroTabs[tabNumber.next].click()
 			// Deactivate current tab
-			// tabs[tabNumber.current].classList.remove('w--tab-active')
-			// Activate new tab
-			// tabs[tabNumber.next].classList.add('w--tab-active')
+			tabs[tabIndex.current].classList.add('fade-out')
 			// Fade in new slide
-			fadeInSlide(tabNumber.next, false)
+			fadeInTab(tabIndex.next)
 			// Reset current slide
-			resetSlide(tabNumber.current)
+			resetSlide(tabIndex.current)
 		})
 	}
 
 	// Fade in first slide
-	fadeInSlide(0, true)
+	fadeInTab(0)
 
 }
