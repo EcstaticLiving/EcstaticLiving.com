487a0e51a54cd9a5d6e24b47ac0ec2d96211334a
Martin Adams <heymartinadams@users.noreply.github.com>
2019-02-23 23:08:48 -0800

    Removed manual scroll.

--- dev/homepage.js
+++ dev/homepage.js
@@ -111,14 +111,4 @@ if (window.location.pathname === '/') {
 		transitionTabs({ currentTab, nextTab: activeTab })
 	}, 8000)
 
-	// Initiate scroll behaviour
-	const welcomeImage = getElementByClassName('welcome-image')
-	welcomeImage.classList.remove('fade-move')
-	const welcomeImageObserve = new IntersectionObserver(() => welcomeImage.classList.add('fade-move'), { threshold: 1.0 })
-	welcomeImageObserve.observe(welcomeImage)
-
-	const welcomeText = getElementByClassName('welcome-text')
-	welcomeText.classList.remove('fade-move')
-	const welcomeTextObserve = new IntersectionObserver(() => setTimeout(() => welcomeText.classList.add('fade-move'), 200), { threshold: 1.0 })
-	welcomeTextObserve.observe(welcomeText)
 }
