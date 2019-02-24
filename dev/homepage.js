19c050fee28afffed370c6a623cc3cada01cf040
Martin Adams <heymartinadams@users.noreply.github.com>
2019-02-23 17:42:56 -0800

    Hero.

--- dev/homepage.js
+++ dev/homepage.js
@@ -77,9 +77,12 @@ if (window.location.pathname === '/') {
 		heroSection.style.height = deviceType() === 'mobile' && deviceOrientation === 'landscape'
 			? Math.max(window.innerHeight * 0.9 + 'px', '550px')
 			: window.innerHeight * 0.9 + 'px'
+		console.log(heroSection.style.height)
 	}
 	else {
 		heroSection.style.height = window.innerHeight * 0.5 + 'px'
+		console.log('yo')
+		console.log(heroSection.style.height)
 	}
 
 	// Add event listener to cycle through all hero messages on arrow click
