e556ff305eff0bf393cdeeb1b0a0d5f66e7f2f7d
Martin Adams <heymartinadams@users.noreply.github.com>
2019-02-25 18:55:01 -0800

    Homepage.

--- dev/homepage.js
+++ dev/homepage.js
@@ -1,22 +1,61 @@
 // Load all tabs
-const tabs = getElementsByClassName('hero-tab')
-const literalNumbers = ['one', 'two', 'three', 'four', 'five', 'six']
-let activeTab = 0
+const ALL_TABS = getElementsByClassName('hero-tab')
+const NUMBER_CLASS = ['one', 'two', 'three', 'four', 'five', 'six']
+let ACTIVE_TAB_INDEX = 0
+
+// Calculate Section.Hero height
+const setHeroHeight = () => {
+	const heroSection = getElementByClassName('section hero')
+	if (
+		deviceType() === 'desktop'
+		|| (deviceType() === 'large tablet' && deviceOrientation() === 'landscape')
+		|| deviceType() === 'tablet'
+		|| deviceType() === 'mobile'
+	) {
+		// Set min height on mobile to 550px
+		heroSection.style.height = deviceType() === 'mobile' && deviceOrientation() === 'landscape'
+			? Math.max(window.innerHeight * 0.95 + 'px', '550px')
+			: window.innerHeight * 0.95 + 'px'
+	}
+	else {
+		heroSection.style.height = window.innerHeight * 0.5 + 'px'
+	}
+}
+
+// Set width of boxes section for width that’s smaller than desktop yet larger than iPad portrait; not configurable in Webflow interface.
+const setBoxSections = () => {
+	const boxSections = getElementsByClassName('section boxes')
+	const boxContainers = getElementsByClassName('box-container')
+	let sectionHeight, containerWidth
+	if (window.innerWidth >= 930 && window.innerWidth < 1240) {
+		sectionHeight = '500px'
+		containerWidth = '768px'
+	}
+	else {
+		sectionHeight = '200px'
+		containerWidth = ''
+	}
+	// Do this for all box section and containers on homepage
+	for (let i = 0; i < boxContainers.length; i++) {
+		boxSections[i].style.height = sectionHeight
+		boxContainers[i].style.width = containerWidth
+	}
+}
 
 // Preload images and randomly rotate
 const fadeInTab = tabIndex => {
 	// Make tab and clickable elements visible
-	tabs[tabIndex].classList.add('display')
+	ALL_TABS[tabIndex].classList.add('display')
 	const heroButton = getElementByClassName('hero-button', tabIndex)
 	const heroArrows = getElementByClassName('hero-arrows', tabIndex)
 	heroButton.classList.add('display')
 	heroArrows.classList.add('display')
 	// Fade in image
-	setTimeout(() => tabs[tabIndex].classList.add('fade'), 100)
+	setTimeout(() => ALL_TABS[tabIndex].classList.add('fade'), 100)
 	// Slide in Hero Text
 	setTimeout(() => {
 		for (let i = 0; i < 3; i++) {
-			const title = getElementByClassName('hero-title ' + literalNumbers[i], tabIndex)
+			const title = getElementByClassName('hero-title ' + NUMBER_CLASS[i], tabIndex)
 			title.classList.add('fade-move')
 		}
 	}, 500)
@@ -29,7 +68,7 @@ const fadeInTab = tabIndex => {
 const resetSlide = tabIndex => {
 	// Hide Hero Text
 	for (let i = 0; i < 3; i++) {
-		const title = getElementByClassName('hero-title ' + literalNumbers[i], tabIndex)
+		const title = getElementByClassName('hero-title ' + NUMBER_CLASS[i], tabIndex)
 		title.classList.remove('fade-move')
 	}
 	// Hide Hero Button
@@ -39,61 +78,23 @@ const resetSlide = tabIndex => {
 	const heroArrows = getElementByClassName('hero-arrows', tabIndex)
 	heroArrows.classList.remove('fade-move')
 	// Make tab and clickable elements invisible
-	tabs[tabIndex].classList.remove('display')
+	ALL_TABS[tabIndex].classList.remove('display')
 	heroButton.classList.remove('display')
 	heroArrows.classList.remove('display')
 }
 
 const transitionTabs = ({ currentTab, nextTab }) => {
 	// Fade out current tab
-	tabs[currentTab].classList.remove('fade')
+	ALL_TABS[currentTab].classList.remove('fade')
 	// Fade in new slide
 	fadeInTab(nextTab)
 	// Reset current slide
 	setTimeout(() => resetSlide(currentTab), 500)
 }
 
+
 // Begin
 if (window.location.pathname === '/') {
-
-	// Calculate Section.Hero height
-	const setHeroHeight = () => {
-		const heroSection = getElementByClassName('section hero')
-		if (
-			deviceType() === 'desktop'
-			|| (deviceType() === 'large tablet' && deviceOrientation() === 'landscape')
-			|| deviceType() === 'tablet'
-			|| deviceType() === 'mobile'
-		) {
-			// Set min height on mobile to 550px
-			heroSection.style.height = deviceType() === 'mobile' && deviceOrientation() === 'landscape'
-				? Math.max(window.innerHeight * 0.95 + 'px', '550px')
-				: window.innerHeight * 0.95 + 'px'
-		}
-		else {
-			heroSection.style.height = window.innerHeight * 0.5 + 'px'
-		}
-	}
-
-	// Set width of boxes section for width that’s smaller than desktop yet larger than iPad portrait; not configurable in Webflow interface.
-	const setBoxSections = () => {
-		const boxSections = getElementsByClassName('section boxes')
-		const boxContainers = getElementsByClassName('box-container')
-		let sectionHeight, containerWidth
-		if (window.innerWidth >= 930 && window.innerWidth < 1240) {
-			sectionHeight = '500px'
-			containerWidth = '768px'
-		}
-		else {
-			sectionHeight = '200px'
-			containerWidth = ''
-		}
-		// Do this for all box section and containers on homepage
-		for (let i = 0; i < boxContainers.length; i++) {
-			boxSections[i].style.height = sectionHeight
-			boxContainers[i].style.width = containerWidth
-		}
-	}
 
 	windowEventListener(['onload', 'orientationchange', 'resize'], () => {
 		// Recalculate hero height on orientation change
@@ -108,19 +109,30 @@ if (window.location.pathname === '/') {
 	for (let tabIndex = 0; tabIndex < rightArrows.length; tabIndex++) {
 		// Cycle forward
 		rightArrows[tabIndex].addEventListener('click', () => {
-			activeTab = tabIndex === tabs.length - 1 ? 0 : tabIndex + 1
-			transitionTabs({ currentTab: tabIndex, nextTab: activeTab })
+			ACTIVE_TAB_INDEX = tabIndex === ALL_TABS.length - 1 ? 0 : tabIndex + 1
+			transitionTabs({ currentTab: tabIndex, nextTab: ACTIVE_TAB_INDEX })
 			// Interrupt auto-cycling if user manually clicked arrow
 			clearInterval(tabInterval)
 		})
 		// Cycle backward
 		leftArrows[tabIndex].addEventListener('click', () => {
-			activeTab = tabIndex === 0 ? tabs.length - 1 : tabIndex - 1
-			transitionTabs({ currentTab: tabIndex, nextTab: activeTab })
+			ACTIVE_TAB_INDEX = tabIndex === 0 ? ALL_TABS.length - 1 : tabIndex - 1
+			transitionTabs({ currentTab: tabIndex, nextTab: ACTIVE_TAB_INDEX })
 			// Interrupt auto-cycling if user manually clicked arrow
 			clearInterval(tabInterval)
 		})
 	}
+
+	// Remove display classes on init
+	ALL_TABS[0].classList.remove('display fade')
+	for (let i = 0; i < 3; i++) {
+		const title = getElementByClassName('hero-title ' + NUMBER_CLASS[i], 0)
+		title.classList.remove('fade-move')
+	}
+	const heroButton = getElementByClassName('hero-button', tabIndex)
+	heroButton.classList.remove('display fade-move')
+	const heroArrows = getElementByClassName('hero-arrows', tabIndex)
+	heroArrows.classList.remove('display fade-move')
 
 	// Set hero height
 	setHeroHeight()
@@ -129,9 +141,9 @@ if (window.location.pathname === '/') {
 
 	// Switch tabs periodically
 	const tabInterval = setInterval(() => {
-		const currentTab = activeTab
-		activeTab = activeTab === tabs.length - 1 ? 0 : activeTab + 1
-		transitionTabs({ currentTab, nextTab: activeTab })
+		const currentTab = ACTIVE_TAB_INDEX
+		ACTIVE_TAB_INDEX = ACTIVE_TAB_INDEX === ALL_TABS.length - 1 ? 0 : ACTIVE_TAB_INDEX + 1
+		transitionTabs({ currentTab, nextTab: ACTIVE_TAB_INDEX })
 	}, 8000)
 
 }
