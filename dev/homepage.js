b6b98f8c63063e4e6381b06667cefcf9e3eb92e0
Martin Adams <heymartinadams@users.noreply.github.com>
2019-02-26 14:09:51 -0800

    Reviews.

--- dev/homepage.js
+++ dev/homepage.js
@@ -1,7 +1,9 @@
-// Load all tabs
-const ALL_TABS = getElementsByClassName('hero-tab')
+// Constants
 const NUMBER_CLASS = ['one', 'two', 'three', 'four', 'five', 'six']
+const ALL_TABS = getElementsByClassName('hero-tab')
 let ACTIVE_TAB_INDEX = 0
+const ALL_REVIEWS = getElementsByClassName('reviews-review')
+let ACTIVE_REVIEW_INDEX = 0
 
 // Calculate Section.Hero height
 const setHeroHeight = () => {
@@ -54,7 +56,7 @@ const fadeInTab = tabIndex => {
 	// Make tab and clickable elements visible
 	ALL_TABS[tabIndex].classList.add('display')
 	const heroButton = getElementByClassName('hero-button', tabIndex)
-	const heroArrows = getElementByClassName('arrows', tabIndex)
+	const heroArrows = getElementByClassName('hero-arrows', tabIndex)
 	heroButton.classList.add('display')
 	heroArrows.classList.add('display')
 	// Fade in image
@@ -72,7 +74,7 @@ const fadeInTab = tabIndex => {
 	setTimeout(() => heroArrows.classList.add('fade-move'), 1100)
 }
 
-const resetSlide = tabIndex => {
+const resetTab = tabIndex => {
 	// Hide Hero Text
 	for (let i = 0; i < 3; i++) {
 		const title = getElementByClassName('hero-title ' + NUMBER_CLASS[i], tabIndex)
@@ -81,13 +83,14 @@ const resetSlide = tabIndex => {
 	// Hide Hero Button
 	const heroButton = getElementByClassName('hero-button', tabIndex)
 	heroButton.classList.remove('fade-move')
-	// Hide Hero Arrows
-	const heroArrows = getElementByClassName('arrows', tabIndex)
-	heroArrows.classList.remove('fade-move')
-	// Make tab and clickable elements invisible
-	ALL_TABS[tabIndex].classList.remove('display')
 	heroButton.classList.remove('display')
+	// Hide Hero Arrows
+	const heroArrows = getElementByClassName('hero-arrows', tabIndex)
+	heroArrows.classList.remove('fade-move')
 	heroArrows.classList.remove('display')
+	// Make tab and clickable elements invisible
+	ALL_TABS[tabIndex].classList.remove('fade')
+	ALL_TABS[tabIndex].classList.remove('display')
 }
 
 const transitionTabs = ({ currentTab, nextTab }) => {
@@ -96,7 +99,40 @@ const transitionTabs = ({ currentTab, nextTab }) => {
 	// Fade in new slide
 	fadeInTab(nextTab)
 	// Reset current slide
-	setTimeout(() => resetSlide(currentTab), 500)
+	setTimeout(() => resetTab(currentTab), 500)
+}
+
+const fadeInReview = reviewIndex => {
+	const reviewsQuoteMark = getElementByClassName('reviews-quote')[reviewIndex]
+	const reviewsQuote = getElementByClassName('reviews-quote')[reviewIndex]
+	const reviewsQuotees = getElementByClassName('reviews-quotee')[reviewIndex]
+	const reviewsArrows = getElementByClassName('reviews-arrows')[reviewIndex]
+	// Display review
+	ALL_REVIEWS[reviewIndex].classList.add('display')
+	setTimeout(() => reviewsQuoteMark.classList.add('fade-move'), 100)
+	setTimeout(() => reviewsQuote.classList.add('fade-move'), 200)
+	setTimeout(() => reviewsQuotees.classList.add('fade-move'), 400)
+	setTimeout(() => reviewsArrows.classList.add('fade-move'), 800)
+}
+
+const resetReview = reviewIndex => {
+	const reviewsQuote = getElementByClassName('reviews-quote')[reviewIndex]
+	const reviewsQuotees = getElementByClassName('reviews-quotee')[reviewIndex]
+	const reviewsArrows = getElementByClassName('reviews-arrows')[reviewIndex]
+	reviewsQuote.classList.remove('fade-move')
+	reviewsQuotees.classList.remove('fade-move')
+	reviewsArrows.classList.remove('fade-move')
+	setTimeout(() => {
+		reviewsArrows.classList.remove('display')
+		ALL_REVIEWS[reviewIndex].classList.remove('display')
+	}, 500)
+}
+
+const transitionReviews = ({ currentReview, nextReview }) => {
+	// Fade out current tab
+	resetReview(currentReview)
+	// Fade in new slide
+	setTimeout(() => fadeInReview(nextReview), 500)
 }
 
 
@@ -111,18 +147,18 @@ if (window.location.pathname === '/') {
 	})
  
 	// Add event listener to cycle through all hero messages on arrow click
-	const leftArrows = getElementsByClassName('arrow left')
-	const rightArrows = getElementsByClassName('arrow right')
-	for (let tabIndex = 0; tabIndex < rightArrows.length; tabIndex++) {
+	const leftHeroArrows = getElementsByClassName('hero-arrow left')
+	const rightHeroArrows = getElementsByClassName('hero-arrow right')
+	for (let tabIndex = 0; tabIndex < rightHeroArrows.length; tabIndex++) {
 		// Cycle forward
-		rightArrows[tabIndex].addEventListener('click', () => {
+		rightHeroArrows[tabIndex].addEventListener('click', () => {
 			ACTIVE_TAB_INDEX = tabIndex === ALL_TABS.length - 1 ? 0 : tabIndex + 1
 			transitionTabs({ currentTab: tabIndex, nextTab: ACTIVE_TAB_INDEX })
 			// Interrupt auto-cycling if user manually clicked arrow
 			clearInterval(tabInterval)
 		})
 		// Cycle backward
-		leftArrows[tabIndex].addEventListener('click', () => {
+		leftHeroArrows[tabIndex].addEventListener('click', () => {
 			ACTIVE_TAB_INDEX = tabIndex === 0 ? ALL_TABS.length - 1 : tabIndex - 1
 			transitionTabs({ currentTab: tabIndex, nextTab: ACTIVE_TAB_INDEX })
 			// Interrupt auto-cycling if user manually clicked arrow
@@ -130,30 +166,52 @@ if (window.location.pathname === '/') {
 		})
 	}
 
-	// Remove display classes on init
-	ALL_TABS[0].classList.remove('fade')
-	ALL_TABS[0].classList.remove('display')
-	for (let i = 0; i < 3; i++) {
-		const title = getElementByClassName('hero-title ' + NUMBER_CLASS[i], 0)
-		title.classList.remove('fade-move')
+	// Add event listener to cycle through all reviews on arrow click
+	const leftReviewsArrows = getElementsByClassName('hero-arrow left')
+	const rightReviewsArrows = getElementsByClassName('hero-arrow right')
+	for (let reviewIndex = 0; reviewIndex < rightReviewsArrows.length; reviewIndex++) {
+		// Cycle forward
+		rightReviewsArrows[reviewIndex].addEventListener('click', () => {
+			ACTIVE_REVIEW_INDEX = reviewIndex === ALL_REVIEWS.length - 1 ? 0 : reviewIndex + 1
+			transitionReviews({ currentReview: reviewIndex, nextReview: ACTIVE_REVIEW_INDEX })
+			// Interrupt auto-cycling if user manually clicked arrow
+			clearInterval(reviewInterval)
+		})
+		// Cycle backward
+		leftReviewsArrows[reviewIndex].addEventListener('click', () => {
+			ACTIVE_REVIEW_INDEX = reviewIndex === 0 ? ALL_REVIEWS.length - 1 : reviewIndex - 1
+			transitionReviews({ currentReview: reviewIndex, nextReview: ACTIVE_REVIEW_INDEX })
+			// Interrupt auto-cycling if user manually clicked arrow
+			clearInterval(reviewInterval)
+		})
 	}
-	const heroButton = getElementByClassName('hero-button', 0)
-	heroButton.classList.remove('fade-move')
-	heroButton.classList.remove('display')
-	const heroArrows = getElementByClassName('arrows', 0)
-	heroArrows.classList.remove('fade-move')
-	heroArrows.classList.remove('display')
+
+	// Remove display classes on init for hero tabs
+	resetTab(0)
+
+	// Remove display classes on init for reviews
+	resetReview(0)
 
 	// Set hero height
 	setHeroHeight()
+
 	// Fade in first slide
 	fadeInTab(0)
+
+	// Fade in first review
+	fadeInReview(0)
 
 	// Switch tabs periodically
 	const tabInterval = setInterval(() => {
 		const currentTab = ACTIVE_TAB_INDEX
 		ACTIVE_TAB_INDEX = ACTIVE_TAB_INDEX === ALL_TABS.length - 1 ? 0 : ACTIVE_TAB_INDEX + 1
 		transitionTabs({ currentTab, nextTab: ACTIVE_TAB_INDEX })
+	}, 8000)
+	// Switch reviews periodically
+	const reviewInterval = setInterval(() => {
+		const currentReview = ACTIVE_REVIEW_INDEX
+		ACTIVE_REVIEW_INDEX = ACTIVE_REVIEW_INDEX === ALL_REVIEWS.length - 1 ? 0 : ACTIVE_REVIEW_INDEX + 1
+		transitionReviews({ currentReview, nextReview: ACTIVE_REVIEW_INDEX })
 	}, 8000)
 
 }
