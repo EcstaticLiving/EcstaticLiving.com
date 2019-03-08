2119295cc2798da57a802f9d7cd0922bf756ca4a
Martin Adams <heymartinadams@users.noreply.github.com>
2019-03-08 13:50:15 -0800

    Reg form modal display.

--- dev/events/init.js
+++ dev/events/init.js
@@ -30,7 +30,7 @@ for (let i = 0; i < regFormButtons.length; i++) {
 		setTimeout(() => regFormModal.style.opacity = '1.0', 200)
 		// Show reg form: browser error that doesn’t show opacity of children unless scrolled to
 		setTimeout(() => {
-			// regFormModalWindow.scrollTo(0, 0)
+			regFormModalWindow.scrollTo(0, 0)
 			regFormModalWindow.onscroll = () => regFormModal.style.opacity = '1.0'
 		}, 300)
 	})
