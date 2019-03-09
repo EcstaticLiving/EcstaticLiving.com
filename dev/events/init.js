6753c0c88a826107fe575b61bfc399f981defc6b
Martin Adams <heymartinadams@users.noreply.github.com>
2019-03-08 16:03:51 -0800

    Reg form modal display.

--- dev/events/init.js
+++ dev/events/init.js
@@ -28,8 +28,7 @@ for (let i = 0; i < regFormButtons.length; i++) {
 			// Scroll to top
 			regFormContainer.scrollTop = 0
 			// Show reg form: browser error that doesn’t show opacity of elements inside modal window unless body is scrolled by at least 1 px
-			window.scrollBy(0, 1)
-			onScroll(regFormContainer, () => regFormModalWindow.style.width = '690px')
+			// window.scrollBy(0, 1)
 			// Prevent background from scrolling
 			document.body.style.overflow = 'hidden'
 		}, 100)
