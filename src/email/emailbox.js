321ca32c750f6f2f4d1f706036c377818728c4fc
Martin Adams <heymartinadams@users.noreply.github.com>
2019-03-14 14:51:07 -0700

    017 - email box functionality

--- src/email/emailbox.js
+++ src/email/emailbox.js
@@ -1,12 +1,12 @@
 // EMAIL NEWSLETTER SIGNUPS
 
 // 1. Define all input fields
-const formNames = [
+const emailBoxNames = [
 	'hero',		// homepage hero email signup
 	'event1',	// events pre-reg (top)
 	'event2'	// events pre-reg (bottom)
 ]
-const formFields = [
+const emailBoxFields = [
 	'first_name',
 	'last_name',
 	'email',
@@ -14,62 +14,54 @@ const formFields = [
 	'phone'				// Only on events page
 ]
 
-// 2. Only once all fields have been entered, enable button.
-const verifyEmailSignup = e => {
-	formNames.forEach(formName => {
-		// If change was made in a certain email signup category...
-		if (e.target.id.includes(formName)) {
-			// ...check to see if every field has been filled out correctly...
-			const complete = formFields.every(formField => {
-				const field = getElementById(formName + '_' + formField)
-				if (formField === 'first_name' || formField === 'last_name') {
-					field.value = standardisationName(field.value)
-				}
-				// return true if all fields have been filled out
-				return (
-					((formField === 'first_name' || formField === 'last_name') && field.value.length > 1)
-					|| (formField === 'email' && field.value.length > 4 && standardisationEmail.test(field.value))
-				)
-			})
-			// Only show alert once every field has been touched...
-			const showAlert = formFields.every(formField => getElementById(formName + '_' + formField).value.length > 0)
-			// ...and if so, change button class to active, and submit form.
-			const buttonField = getElementById(formName + '_button')
-			const alertField = getElementById(formName + '_alert')
-			// Only show alert if all fields have been filled out somewhat, but not yet validated
-			if (showAlert) {
-				alertField.classList.remove('hidden')
-			}
-			else {
-				alertField.classList.add('hidden')
-			}
-			const form = getElementById(formName + '_form')
-			if (complete && !showAlert) {
-				buttonField.classList.remove('disabled')
-				form.action = 'https://app.getresponse.com/add_subscriber.html'
-				buttonField.addEventListener('click', () => form.submit())
-			}
-			// ...otherwise, disable form.
-			else {
-				buttonField.classList.add('disabled')
-				form.action = ''
-				form.removeEventListener('click', () => null)
-			}
-		}
-	}) 
-}
-
-// 3. Assign enable button function to all relevant fields, and trigger submit if user presses enter
-formNames.forEach(formName => {
-	formFields.forEach(formField => {
-		const field = getElementById(formName + '_' + formField)
+// 2. Assign enable button function to all relevant fields, and trigger submit if user presses enter
+emailBoxNames.forEach(emailBoxName => {
+	emailBoxFields.forEach(emailBoxField => {
+		const field = getElementById(emailBoxName + '_' + emailBoxField)
 		if (field) {
 			field.oninput = verifyEmailSignup
 			field.addEventListener('keypress', e => {
+
+				// Check to see if every field has been filled out correctly...
+				const valid = emailBoxFields.every(checkEmailBoxField => {
+					const field = getElementById(emailBoxName + '_' + checkEmailBoxField)
+					// If field doesn’t exist, return true.
+					if (!field)	return true
+					// Make sure first and last names don’t contain spaces
+					if (checkEmailBoxField === 'first_name' || checkEmailBoxField === 'last_name') {
+						field.value = standardisationName(field.value)
+					}
+					// return true if all fields have been filled out
+					return (
+						// First and last names have to have at least two characters each...
+						((checkEmailBoxField === 'first_name' || checkEmailBoxField === 'last_name') && field.value.length > 1)
+						// ...email has to be at least 4 characters and valid...
+						&& (checkEmailBoxField === 'email' && field.value.length > 4 && standardisationEmail.test(field.value))
+					)
+				})
+				// Only show alert once every field has been touched...
+				const everyFieldHasBeenFilledOut = emailBoxFields.every(checkEmailBoxField => getElementById(emailBoxName + '_' + checkEmailBoxField).value.length > 0)
+				const buttonField = getElementById(emailBoxName + '_button')
+				const alertField = getElementById(emailBoxName + '_alert')
+				const form = getElementById(emailBoxName + '_form')
+				// Only show alert if all fields have been filled out somewhat, but not yet validated
+				if (!valid && everyFieldHasBeenFilledOut) {
+					alertField.classList.remove('hidden')
+					buttonField.classList.add('disabled')
+					form.action = ''
+					form.removeEventListener('click', () => null)
+				}
+				// ...and if all’s good to go, change button class to active, and submit form.
+				else {
+					alertField.classList.add('hidden')
+					buttonField.classList.remove('disabled')
+					form.action = 'https://app.getresponse.com/add_subscriber.html'
+					buttonField.addEventListener('click', () => form.submit())
+				}
 				const key = e.which || e.keyCode
 				// If `enter` key is pressed, attempt to submit form
 				if (key === 13) {
-					const buttonField = getElementById(formName + '_button')
+					const buttonField = getElementById(emailBoxName + '_button')
 					buttonField.click()
 				}
 			})
