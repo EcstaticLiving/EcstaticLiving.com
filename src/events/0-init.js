69afc9f528071b434f48dd1b2ea6c4375db32a8c
Martin Adams <heymartinadams@users.noreply.github.com>
2019-03-15 12:09:08 -0700

    003 - removed 3-emailbox

--- src/events/0-init.js
+++ src/events/0-init.js
@@ -1,14 +1,14 @@
 if (page() === 'Event' || page() === 'Update') {
 	// Load reg form scripts
 	const baseScriptUrl = 'https://ecstaticliving.github.io/ecstaticliving.com/src/events/'
-	const scripts = ['1-design', '2-elements', '3-emailbox', '4-functions', '5-onchange', '6-webflow', '7-payment']
+	const scripts = ['1-design', '2-elements', '3-functions', '4-onchange', '5-webflow', '6-payment']
 	// Load scripts synchronously, one after another, to make sure hierarchy is respected.
 	const loadScript = index => {
 		let js = document.createElement('script')
 		js.src = baseScriptUrl + scripts[index] + '.js'
 		js.onload = () => {
 			// Once final script has loaded, initialise
-			if (scripts[index] === '7-payment') {
+			if (scripts[index] === '6-payment') {
 				resetForm()
 			}
 			else {
