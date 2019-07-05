b37bd67892612c6a2cbd586bd0933ebfd613e70e
Martin Adams <heymartinadams@users.noreply.github.com>
2019-07-05 15:36:24 -0700

    Adding recaptcha.

--- recaptcha/recaptcha.js
+++ recaptcha/recaptcha.js
@@ -1,8 +1,9 @@
 // Uses webtask.io
-// wt create recaptcha.js --secret secretkey=6LcqUawUAAAAAF1SV-nUtCpCjZ_QFSY3Rd0dxtzV --meta 'wt-node-dependencies'='{"grecaptcha":"latest"}'
+// wt create ./recaptcha/recaptcha.js --secret secretkey=6LcqUawUAAAAAF1SV-nUtCpCjZ_QFSY3Rd0dxtzV --meta 'wt-node-dependencies'='{"grecaptcha":"latest"}'
 const Grecaptcha = require('grecaptcha')
 
 module.exports = (body, callback) => {
+  console.log('recaptcha')
   const client = new Grecaptcha(body.secrets.secretkey)
   if (await client.verify(body.data.token)) {
     console.log('accepted')
