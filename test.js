e6fdcd9c97025b31aa6311ff9090c791913a3848
Martin Adams <heymartinadams@users.noreply.github.com>
2019-07-08 15:28:08 -0700

    Recaptcha.

--- test.js
+++ test.js
@@ -10,8 +10,10 @@ if (mode === 'production') {
 	console.log('TEST code at ', window.location.href)
 }
 
-// Recaptcha
-const emailNewsletters = [
+// EMAIL NEWSLETTER SIGNUPS
+
+// 1. Define all input fields
+const inputCategories = [
 	'questions_tantra',
 	'questions_relationships',
 	'questions_holistic',
@@ -19,22 +21,100 @@ const emailNewsletters = [
 	'community',
 	'contact'
 ]
-const enableButtons = mode => {
-	emailNewsletters.forEach(emailNewsletter => {
-		const newsletterButton = document.getElementById(emailNewsletter + '_button')
-		if (newsletterButton && mode) {
-			newsletterButton.disabled = false
-			newsletterButton.classList.remove('disabled')
-		} else if (newsletterButton && !mode) {
-			console.log(emailNewsletter)
-			const alert = document.getElementById(emailNewsletter + '_alert')
-			if (alert) alert.innerHTML = 'Please contact our office.'
-			newsletterButton.disabled = true
-			newsletterButton.classList.add('disabled')
+const inputFields = ['first_name', 'last_name', 'email']
+
+// 2. Only once all fields are entered and recaptcha passed, enable button.
+const verifyEmailSignup = e => {
+	inputCategories.forEach(inputCategory => {
+		// If change was made in a certain email signup category...
+		if (e.target.id.includes(inputCategory)) {
+			// ...check to see if every field has been filled out correctly...
+			const complete = inputFields.every(inputField => {
+				const field = document.getElementById(inputCategory + '_' + inputField)
+				if (inputField === 'first_name' || inputField === 'last_name') {
+					// Prevent ALL CAPS
+					field.value = field.value.toLowerCase()
+					// Title case
+					field.value = field.value.charAt(0).toUpperCase() + field.value.slice(1)
+					// No empty spaces
+					if (field.value.includes(' ')) {
+						field.value = field.value.replace(' ', '')
+					}
+				}
+				const emailRegex = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/gim
+				// return true if all fields have been filled out
+				return (
+					((inputField === 'first_name' || inputField === 'last_name') && field.value.length > 1) ||
+					(inputField === 'email' && emailRegex.test(field.value))
+				)
+			})
+
+			const showAlert = inputFields.every(inputField => {
+				const field = document.getElementById(inputCategory + '_' + inputField)
+				return (
+					// Only show alert once every field has been touched...
+					field.value.length > 0 &&
+					// ...and other fields (except for email) are incomplete.
+					inputField !== 'email' &&
+					!complete
+				)
+			})
+
+			// ...and if so, change button class to active, and submit form.
+			const buttonField = document.getElementById(inputCategory + '_button')
+			const alertField = document.getElementById(inputCategory + '_alert')
+
+			// If recaptcha failed
+			alertField.innerHTML = !recaptcha
+				? 'Please contact our office.'
+				: 'Hmm... something’s not quite right'
+
+			// Only show alert if all fields have been filled out somewhat, but not yet validated
+			if (showAlert) {
+				alertField.classList.remove('hidden')
+			} else {
+				alertField.classList.add('hidden')
+			}
+
+			// ENABLE
+			if (complete && !showAlert) {
+				buttonField.classList.remove('disabled')
+				const formField = document.getElementById(inputCategory + '_form')
+				formField.action = 'https://app.getresponse.com/add_subscriber.html'
+				buttonField.addEventListener('click', () => formField.submit())
+			}
+			// DISABLE
+			else {
+				buttonField.classList.add('disabled')
+				const formField = document.getElementById(inputCategory + '_form')
+				formField.action = ''
+				buttonField.removeEventListener('click', () => null)
+			}
 		}
 	})
 }
 
+const newsletterSignups = () => {
+	// Question newsletter signups
+	inputCategories.forEach(inputCategory => {
+		inputFields.forEach(inputField => {
+			const field = document.getElementById(inputCategory + '_' + inputField)
+			if (field) {
+				field.oninput = verifyEmailSignup
+				field.addEventListener('keypress', e => {
+					const key = e.which || e.keyCode
+					// If `enter` key is pressed, attempt to submit form
+					if (key === 13) {
+						const buttonField = document.getElementById(inputCategory + '_button')
+						buttonField.click()
+					}
+				})
+			}
+		})
+	})
+}
+
+let recaptcha = false
 const recaptchaServer =
 	'https://wt-d2bd89d1d23e6c320f5aff229c206923-0.sandbox.auth0-extend.com/recaptcha'
 grecaptcha.ready(function() {
@@ -52,16 +132,14 @@ grecaptcha.ready(function() {
 				// Success
 				.then(function(res) {
 					console.log(res)
-					if (res && res.success && res.score > 0.8) {
-						enableButtons(true)
-					} else {
-						enableButtons(false)
-					}
+					recaptcha = res && res.success && res.score > 0.8 ? true : false
+					newsletterSignups()
 				})
 				// Failure
 				.catch(function(err) {
 					console.error(err)
-					enableButtons(false)
+					recaptcha = false
+					newsletterSignups()
 				})
 		})
 })
